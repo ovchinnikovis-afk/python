0f3330c9d763b68396b5d99436ea652f89e7c6fa
ovchinnikovis-afk <ovchinnikovis@mer.ci.nsu.ru>
2025-10-08 17:15:21 +0700

    Delete 1 directory

--- 1/kvadrat.py
+++
@@ -1,8 +0,0 @@
-import math
-num = int(input("Введите целое число: "))
-if num >= 0:
-    result = math.sqrt(num)
-    print("корень числа=",(result))
-else:
-    result = num * num 
-    print("квадрат числа=",(result))
